f8b60c377105f1c7e267ff5f721b3e69db681f98
Juan Pablo Addeo <jpaddeo@gmail.com>
2025-06-09 14:43:27 -0300

    style: :art: re-style not found page

--- reactjs/entrega2/src/pages/NotFound.jsx
+++ reactjs/entrega2/src/pages/NotFound.jsx
@@ -1,12 +1,19 @@
+import { Link } from 'react-router';
+
 export default function NotFoundPage() {
   return (
-    <div className='flex items-center justify-center flex-col h-screen bg-gray-100'>
-      <h1 className='text-4xl font-bold text-gray-800'>
-        404 - Página no encontrada
-      </h1>
-      <p className='mt-4 text-lg text-gray-600'>
-        Lo sentimos, la página que buscas no existe.
-      </p>
+    <div className='mx-auto max-w-screen-xl px-4 2xl:px-0'>
+      <div className='flex flex-col items-center justify-center p-8 text-center bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700'>
+        <h2 className='mb-2 text-xl font-semibold text-gray-900 dark:text-white'>
+          No se encontró la página que buscas
+        </h2>
+        <Link
+          to='/'
+          className='text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800'
+        >
+          Volver al inicio
+        </Link>
+      </div>
     </div>
   );
 }
