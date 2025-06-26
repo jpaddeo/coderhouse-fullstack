24a1f07a0fd2e16d4a4e3cb3e8403f5fadabacc0
Juan Pablo Addeo <jpaddeo@gmail.com>
2025-06-26 10:00:01 -0300

    feat: :zap: add one time service initialization

--- backend_1/entrega1/src/server.js
+++ backend_1/entrega1/src/server.js
@@ -3,6 +3,9 @@ import express from 'express';
 import productsRouter from './routes/products.js';
 import cartsRouter from './routes/carts.js';
 
+import productsService from './services/products.js';
+import cartsService from './services/carts.js';
+
 const app = express();
 
 app.use(express.json());
@@ -12,6 +15,12 @@ const PORT = process.env.PORT || 5000;
 app.use('/api/products', productsRouter);
 app.use('/api/carts', cartsRouter);
 
-app.listen(PORT, () => {
-  console.log(`Entrega 1 server is running on http://localhost:${PORT}`);
+app.listen(PORT, async () => {
+  console.log('[INICIO] - Validación y/o creación de archivo de datos de productos.');
+  await productsService.init();
+  console.log('[FIN] - Validación y/o creación de archivo de datos de productos.');
+  console.log('[INICIO] - Validación y/o creación de archivo de datos de carritos.');
+  await cartsService.init();
+  console.log('[FIN] - Validación y/o creación de archivo de datos de carritos.');
+  console.log(`[SERVER ON] - El servidor de la ENTREGA 1 está ejecutándose en http://localhost:${PORT}`);
 });
