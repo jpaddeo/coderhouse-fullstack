4534ab8dc8f4a6c51a9b1e7613dc3723f66c6be1
Juan Pablo Addeo <jpaddeo@gmail.com>
2025-07-02 11:12:06 -0300

    fix: :bug: fix build errors setting vitejs to allow top level awaits

--- reactjs/entrega_final/vite.config.js
+++ reactjs/entrega_final/vite.config.js
@@ -5,6 +5,14 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
+  optimizeDeps: {
+    esbuildOptions: {
+      target: 'esnext',
+    },
+  },
+  build: {
+    target: 'esnext',
+  },
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
